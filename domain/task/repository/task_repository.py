77c09f6b07dda3f626392c7a82356b72152c9386
wlswnsdn <wlswnsdn123@gmail.com>
2025-07-16 23:08:54 +0900

    [FEAT] Report, Task 레포지토리 추가

---
+++ domain/task/repository/task_repository.py
@@ -0,0 +1,8 @@
+from core.database.model.task import Task
+from core.database.repository.crud_repository import CRUDRepository
+
+
+class TaskRepository(CRUDRepository[Task]):
+    def model_class(self) -> type[Task]:
+        """Task 모델 클래스를 반환합니다."""
+        return Task
