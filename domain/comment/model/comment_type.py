5927be53af31964cbfd9431c48e58c88d0ffd773
MINHYUK SONG <dnfldpden32@gmail.com>
2025-08-03 14:30:12 +0900

    REFACTOR : 댓글 분류 +요약 기능 구현 (#37)
    * feat:댓글 감정 분류 프롬포트 작성
    
    * feat:댓글 저장을 위한 레포지토리 생성
    
    * feat:프롬포트 수정
    
    * feat:댓글분석 로직 구현
    
    * feat:댓글 풀링->저장->로직 서비스 구현
    
    * chore: comment_service 이름 변경
    
    * fix: 분류 로직 오류 해결
    
    * feat: 댓글 분류후 레포트 댓글 카운트 업데이트
    
    * feat: 프롬포트 설정
    
    * feat: llm 로직 설계
    
    * feat: 요약된 comments 저장 로직 구현
    
    * feat: 전체 로직 적용

--- domain/comment/model/comment_type.py
+++ domain/comment/model/comment_type.py
@@ -1,6 +1,18 @@
 from enum import Enum
+
+
 class CommentType(str, Enum):
-    ADVICE_OPINION = 'ADVICE_OPINION'
-    NEGATIVE = 'NEGATIVE'
-    NEUTRAL = 'NEUTRAL'
-    POSITIVE = 'POSITIVE'
+    ADVICE_OPINION = 'advice_opinion'
+    NEGATIVE = 'negative'
+    NEUTRAL = 'neutral'
+    POSITIVE = 'positive'
+
+    @staticmethod
+    def from_emotion_code(code: int) -> "CommentType":
+        mapping = {
+            1: CommentType.POSITIVE,
+            2: CommentType.NEGATIVE,
+            3: CommentType.NEUTRAL,
+            4: CommentType.ADVICE_OPINION
+        }
+        return mapping.get(code, CommentType.NEUTRAL)  # 기본값 NEUTRAL
--- domain/report/repository/report_repository.py
+++ domain/report/repository/report_repository.py
@@ -5,6 +5,15 @@ class ReportRepository(CRUDRepository[Report]):
     def model_class(self) -> type[Report]:
         """Report 모델 클래스를 반환합니다."""
         return Report
+
+    async def update_count(self, report_id, count_dict):
+        report = await self.find_by_id(report_id)
+        if not report:
+            return False
+        for comment_type, count in count_dict.items():
+            setattr(report, f"{comment_type.value}_comment", count)
+        await self.save(report.dict())
+        return True
     
 
     
